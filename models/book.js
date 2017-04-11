32a7cc7b02d06437edd3943d371b38c3986ebc4d
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-11 16:37:03 -0700

    basic book.js file

---
+++ models/book.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new Schema({
+  title: String,
+  author: String,
+  image: String,
+  release_date: String
+});
+
+var Book = mongoose.model('Book', BookSchema);
+
+
+// to export Book from this module (that's this file)
+// var Book = mongoose.model('Book', BookSchema);
+module.exports = Book;
