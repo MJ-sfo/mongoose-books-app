4912be90eb18ef7fdfa5f52da845332bce18bf82
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-08 18:49:04 -0700

    starting over

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,69 +32,16 @@ $(document).ready(function(){
       error: deleteBookError
     });
   });
-  //  this is from solutions
-  $booksList.on('submit', '#addCharacterForm', function(e) {
-    e.preventDefault();
-    console.log('new characters');
-    $.ajax({
-      method: 'POST',
-      // url: '/api/books/'+$(this).attr('data-id')+'/characters',
-      url: '/api/books', //  should match url in server.js post
-      // data: $(this).serializeArray(),
-      data: $(this).serializeArray(),
-      success: newCharacterSuccess,
-      error: newCharacterError
-    });
-  });
-});  //  ALL $booksList functions need to be within this curly-bracket
 
-// this is what i started with
-// function getBookHtml(book) {
-  // if change ${book.author.name}, makes webpage blank out
-//   return `<hr>
-//           <p>
-//             <b>${book.title}</b>
-//             by ${book.author}
-//             <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${book._id}>Delete</button>
-//           </p>`;
-// }
-
-
-
-
-// });
-
-function getCharacterHtml(_book_id, character) {
-return `${character.name} <button class="deleteCharacter btn btn-danger" data-bookid=${_book_id} data-charid=${character._id}><b>x</b></button>`;
-}
-
-function getAllCharactersHtml(_book_id, characters) {
-if (characters) {
-  return characters.map(function(character) {
-              return getCharacterHtml(_book_id, character);
-            }).join("");
-} else {
-  return "";
-}
-}
+});
 
 function getBookHtml(book) {
   return `<hr>
           <p>
             <b>${book.title}</b>
-            by ${(book.author) ? book.author.name : 'null'}
-            <br>
-            <b>Characters:</b>
-            ${getAllCharactersHtml(book.id, book.characters)}
+            by ${book.author}
             <button type="button" name="button" class="deleteBtn btn btn-danger pull-right" data-id=${book._id}>Delete</button>
-          </p>
-          <form class="form-inline" id="addCharacterForm" data-id=${book._id}>
-            <div class="form-group">
-              <input type="text" class="form-control" name="name" placeholder="Book character">
-            </div>
-            <button type="submit" class="btn btn-default">Add character</button>
-          </form>
-          `;
+          </p>`;
 }
 
 function getAllBooksHtml(books) {
@@ -151,22 +98,4 @@ function deleteBookSuccess(json) {
 
 function deleteBookError() {
   console.log('deletebook error!');
-}
-
-// added from solutions
-function newCharacterSuccess(json) {
-  var book = json;
-  var bookId = book._id;
-  // find the book with the correct ID and update it
-  for(var index = 0; index < allBooks.length; index++) {
-    if(allBooks[index]._id === bookId) {
-      allBooks[index] = book;
-      break;  // we found our book - no reason to keep searching (this is why we didn't use forEach)
-    }
-  }
-  render();
-}
-
-function newCharacterError() {
-  console.log('adding new character error!');
 }
--- server.js
+++ server.js
@@ -1,24 +1,14 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
+
+/////////////////////////////
+//  SETUP and CONFIGURATION
+/////////////////////////////
+
 //require express in our app
 var express = require('express'),
-  bodyParser = require('body-parser'),
-  db = require('./models');
-
-  // from Brianna 4/13
-  // custom middleware to console.log some helpful information
-//   in terminal every time we get a request
-function logRequestInfo(req, res, next){
-  console.log(`\nRECEIVED REQUEST : ${req.method} ${req.url}`);
-  console.log('query params:', req.query);
-  console.log('body:', req.body);
-  // request url parameters haven't been decided yet
-  //  so we'll have to log them inside any routes where
-  //  we want to use them
-  next();
-}
-app.use(logRequestInfo);
+  bodyParser = require('body-parser');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -29,6 +19,52 @@ app.use(express.static('public'));
 // body parser config to accept our datatypes
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+////////////////////
+//  DATA
+///////////////////
+
+var books = [
+  {
+    _id: 15,
+    title: "The Four Hour Workweek",
+    author: "Tim Ferriss",
+    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
+    release_date: "April 1, 2007"
+  },
+  {
+    _id: 16,
+    title: "Of Mice and Men",
+    author: "John Steinbeck",
+    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
+    release_date: "Unknown 1937"
+  },
+  {
+    _id: 17,
+    title: "Romeo and Juliet",
+    author: "William Shakespeare",
+    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
+    release_date: "Unknown 1597"
+  }
+];
+
+
+var newBookUUID = 18;
+
+
+
+
+
+
+
+////////////////////
+//  ROUTES
+///////////////////
+
+
+
+
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
@@ -36,77 +72,67 @@ app.get('/', function (req, res) {
 
 // get all books
 app.get('/api/books', function (req, res) {
-  // find one book by its id
-  db.Book.find({})
-    .populate('author')
-    .exec(function(err, books){
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      res.json(books);
-    });
-
+  // send all books as JSON response
+  console.log('books index');
+  res.json(books);
 });
 
-
-
+// get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  db.Book.findById(req.params.id)
-    // populate the author
-    .populate('author')
-    .exec(function(err, book){
-      if (err) {
-        res.status(500).send(err);
-        return;
-      }
-      res.json(book);
-    });
-
+  console.log('books show', req.params);
+  for(var i=0; i < books.length; i++) {
+    if (books[i]._id === req.params.id) {
+      res.json(books[i]);
+      break; // we found the right book, we can stop searching
+    }
+  }
 });
 
-
-
+// create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  var newBook = new db.Book({
-    title: req.body.title,
-    image: req.body.image,
-    releaseDate: req.body.releaseDate,
-  });
-  // find the author from req.body
-  db.Author.findOne({name: req.body.author}, function(err, author){
-    if (err) {
-      return console.log(err);
-    }
-    // add this author to the book
-    newBook.author = author;
-    // save newBook to database
-    newBook.save(function(err, book){
-      if (err) {
-        return console.log("save error: " + err);
-      }
-      console.log("saved ", book.title);
-      // send back the book!
-      res.json(book);
-    });
-  });
-
+  console.log('books create', req.body);
+  var newBook = req.body;
+  newBook._id = newBookUUID++;
+  books.push(newBook);
+  res.json(newBook);
 });
 
+// update book
+app.put('/api/books/:id', function(req,res){
+// get book id from url params (`req.params`)
+  console.log('books update', req.params);
+  var bookId = req.params.id;
+  // find the index of the book we want to remove
+  var updateBookIndex = books.findIndex(function(element, index) {
+    return (element._id === parseInt(req.params.id)); //params are strings
+  });
+  console.log('updating book with index', deleteBookIndex);
+  var bookToUpdate = books[deleteBookIndex];
+  books.splice(updateBookIndex, 1, req.params);
+  res.json(req.params);
+});
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
   // get book id from url params (`req.params`)
-  console.log(req.params)
+  console.log('books delete', req.params);
   var bookId = req.params.id;
-
-  db.Book.findOneAndRemove({ _id: bookId }, function (err, deletedBook) {
-    res.json(deletedBook);
+  // find the index of the book we want to remove
+  var deleteBookIndex = books.findIndex(function(element, index) {
+    return (element._id === parseInt(req.params.id)); //params are strings
   });
+  console.log('deleting book with index', deleteBookIndex);
+  var bookToDelete = books[deleteBookIndex];
+  books.splice(deleteBookIndex, 1);
+  res.json(bookToDelete);
 });
 
+
+
+
+
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening at http://localhost:3000/');
+  console.log('Book app listening at http://localhost:3000/');
 });
